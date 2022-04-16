32e121d359782bb5bac05169ff6f138184fbc2ce
thegergo02 <thegergo02@freeself.one>
2022-04-16 12:35:47 +0200

    refactor: better collection pagination

---
+++ lib/misc/collection_list_view.dart
@@ -0,0 +1,27 @@
+import 'package:easyt/data/data.dart';
+import 'package:easyt/misc/collection_tile.dart';
+import 'package:easyt/misc/named_type_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+class CollectionListView extends StatelessWidget {
+  final PagingController<String, CollectionReference> controller;
+  const CollectionListView({Key? key, required this.controller})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PagedListView<String, CollectionReference>.separated(
+      pagingController: controller,
+      padding: const EdgeInsets.all(16),
+      builderDelegate: // TODO: error indicators
+          PagedChildBuilderDelegate(itemBuilder: (context, collectionReference, i) {
+        return CollectionTile(collectionId: collectionReference.id);
+      }),
+      separatorBuilder: (context, i) => const Divider(
+        thickness: 2,
+        color: Color.fromARGB(255, 0, 0, 0),
+      ), // TODO: theme
+    );
+  }
+}
--- lib/screens/collections_screen.dart
+++ lib/screens/collections_screen.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easyt/data/data.dart';
 import 'package:easyt/data/provider.dart';
+import 'package:easyt/misc/collection_list_view.dart';
 import 'package:easyt/misc/collection_tile.dart';
 import 'package:easyt/routes/router.gr.dart';
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:provider/provider.dart';
 
 // TODO: switch to infinite_scroll_pagination
@@ -16,13 +18,13 @@ class CollectionsScreen extends StatefulWidget {
 }
 
 class _CollectionsScreenState extends State<CollectionsScreen> {
-  final _collectionReferences = <CollectionReference>[];
-  late ScrollController _controller;
+  final _controller =
+      PagingController<String, CollectionReference>(firstPageKey: "");
 
   @override
   void initState() {
     super.initState();
-    _controller = ScrollController()..addListener(_scrollListener);
+    _controller.addPageRequestListener((pageKey) => _fetchPage(pageKey));
   }
 
   @override
@@ -31,52 +33,38 @@ class _CollectionsScreenState extends State<CollectionsScreen> {
     super.dispose();
   }
 
-  Widget _buildCollection(CollectionReference collection) {
-    return CollectionTile(collectionId: collection.id);
-  }
-
-  void _scrollListener() {
-    if (_controller.position.extentAfter < 500) {
-      _collectionReferences.addAll(
-          Provider.of<DataProvider>(context, listen: false)
-              .getCollectionReferences(10, _getLastId()));
-    }
-  }
-
-  Widget _buildCollections() {
-    return ListView.builder(
-      controller: _controller,
-      padding: const EdgeInsets.all(16),
-      itemCount: _collectionReferences.length * 2, // NOTE: cos divider...
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return const Divider();
-        }
-
-        final index = i ~/ 2;
-        return _buildCollection(_collectionReferences[index]);
-      },
-    );
-  }
-
   void _createCollection() {
     AutoRouter.of(context).push(const CreateCollectionRoute());
   }
 
-  String _getLastId() {
-    if (_collectionReferences.isNotEmpty) {
-      return _collectionReferences.last.id;
+  void _fetchPage(String pageKey) {
+    const int size = 10;
+    try {
+      final List<CollectionReference> collectionReferencePage =
+          Provider.of<DataProvider>(context, listen: false)
+              .getCollectionReferences(size, pageKey);
+      if (collectionReferencePage.length < size) {
+        _controller.appendLastPage(collectionReferencePage);
+      } else {
+        _controller.appendPage(
+            collectionReferencePage, collectionReferencePage.last.id);
+      }
+    } catch (error) {
+      _controller.error = error;
     }
-    return "";
+  }
+
+  void _prepareListener() {
+    Provider.of<DataProvider>(context).addListener(() => _controller.refresh());
   }
 
   @override
   Widget build(BuildContext context) {
-    _collectionReferences.clear();
-    _collectionReferences.addAll(
-        Provider.of<DataProvider>(context).getCollectionReferences(10, ""));
+    _prepareListener();
     return Scaffold(
-      body: _buildCollections(),
+      body: RefreshIndicator(
+          onRefresh: () => Future.sync(() => _controller.refresh()),
+          child: CollectionListView(controller: _controller)),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: _createCollection,
