e328dabd74fbab098ce72523d0f9e88cf47fa4ba
thegergo02 <work@varigergo.xyz>
2022-08-14 14:58:00 +0200

    fix: nix

---
+++ nix/android.nix
@@ -0,0 +1,18 @@
+{ androidenv }:
+
+androidenv.composeAndroidPackages {
+  #toolsVersion = "25.2.5";
+  #platformToolsVersion = "29.0.6";
+  buildToolsVersions = [ "29.0.2" ];
+  includeEmulator = true;
+  emulatorVersion = "31.3.9";
+  platformVersions = [ "31" ];
+  includeSources = false;
+  #includeDocs = false;
+  includeSystemImages = true;
+  systemImageTypes = [ "default" ];
+  abiVersions = [ "x86_64" ];
+  includeNDK = false;
+  useGoogleAPIs = false;
+  useGoogleTVAddOns = false;
+}
---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ sources ? import ./nix/sources.nix }:
+
+let
+  pkgs = import sources.nixpkgs { config.android_sdk.accept_license = true; config.allowUnfree = true; };
+  android = pkgs.callPackage ./nix/android.nix { };
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    # from pkgs
+    flutter
+    unzip
+    jdk11
+    niv
+    #from ./nix/*
+    android.platform-tools ];
+
+  ANDROID_HOME = "${android.androidsdk}/libexec/android-sdk";
+  JAVA_HOME = pkgs.jdk11;
+  ANDROID_AVD_HOME = (toString ./.) + "/.android/avd";
+}
